48add5b23ca236bc538a1663bfa841a297236834
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-21 14:31:18 +0300

    all is working fine, but ? because i use HashSet the output strings are not in that order as server expect (even all the strings are correctly generated)

--- src/bin/creating_strings.rs
+++ src/bin/creating_strings.rs
@@ -1,36 +1,79 @@
 use std::io::{BufRead, BufReader};
+use std::collections::HashSet;
+use std::collections::BTreeMap;
+
+
+
+
+pub struct Perm {
+    pub n:usize,
+    pub line:String,
+    pub permutation:String,
+    pub chosen:[bool;8],
+    pub se:HashSet<String>
+}
+
+impl Perm {
+    fn new(n:&usize
+           ,line:&String
+           ,perm:&String
+           ,chosen:&[bool;8]
+           ,se:&HashSet<String>) -> Perm {
+        Perm {
+            n:n.to_owned(),
+            line:line.to_owned(),
+            permutation:perm.to_owned(),
+            chosen:chosen.to_owned(),
+            se:se.to_owned(),
+
+        }
+    }
+    fn search(&mut self) {
+        if self.permutation.len() == self.n {
+//            println!("length {}", &self.permutation.len());
+//            println!("will be inserted {}\n", &self.permutation.to_string());
+            self.se.insert(self.permutation.to_string());
+        } else {
+            for i in 0..self.n {
+                if self.chosen[i] {continue};
+                self.chosen[i]=true;
+                self.permutation.push(self.line.chars().nth(i).unwrap());
+                self.search();
+                self.chosen[i]=false;
+                self.permutation = self.permutation[0..self.permutation.len()-1].to_string();
+            }
+        }
+/*
+        println!("{}",&self.se.len());
+        for el in self.se.iter() {
+            println!("{}",&el);
+        }
+*/
+    }
+}
 
 fn main() {
     let mut input = BufReader::new(std::io::stdin());
     let mut line = "".to_string();
+    let se:HashSet<String> = HashSet::new();
+    let permutation = "".to_string();
+    let chosen: [bool;8] = [false;8];
+
     input.read_line(&mut line)
         .unwrap();
-    let size = line.len();
-    if (size < 1 ) || (size > 8 ) {
+// remove the trailing '\n'
+    line.pop();
+// max size of input -> see task constrain
+    let n_max = 8;
+    let n = line.len();
+    if (n < 1 ) || (n > n_max ) {
         panic!("Not valid input string size! Panic!")
     }
+    let mut ob:Perm = Perm::new(&n,&line,&permutation,&chosen,&se);
+    ob.search();
+    print!("{}\n",&ob.se.len());
 
-//    let mut a: i64 = split.next().unwrap().parse().unwrap();
-    print!("{} ",line);
-/*
-    while a != 1 && 1 < a {
-        if a % 2 == 0 {
-            a = a/2;
-        } else {
-            a = a*3 +1
-        }
-
-        if 1 <= a {
-            print!("{} ",a);
-        }
+    for el in ob.se.iter() {
+        print!("{}\n",&el);
     }
-*/
-    let set:HashSet<char> = HashSet::with_capacity(size);
-    for ch in line.chars() {
-        
-    }
-
-    let collector:HashSet<char> = HashSet::new();
-    for lenght in 
-
 }
