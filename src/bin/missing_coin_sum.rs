55e7eaab2a94270eb97eb13445a83178d54ee00d
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-24 11:37:53 +0300

    read data

--- src/bin/missing_coin_sum.rs
+++ src/bin/missing_coin_sum.rs
@@ -1,22 +1,27 @@
 use std::io::{BufRead, BufReader};
+use std::collections::BTreeMap;
+use std::io;
+use std::str::SplitWhitespace;
+
 
 fn main() {
     let mut input = BufReader::new(std::io::stdin());
     let mut line = "".to_string();
     input.read_line(&mut line).unwrap();
+    line.pop();
+    let number = line.parse::<u64>().unwrap();
+
+    let mut input = BufReader::new(std::io::stdin());
+    let mut line = "".to_string();
+    input.read_line(&mut line).unwrap();
+    line.pop();
+    
     let mut split = line.split_whitespace();
-    let mut a: i64 = split.next().unwrap().parse().unwrap();
-    print!("{} ",a);
-
-    while a != 1 && 1 < a {
-        if a % 2 == 0 {
-            a = a/2;
-        } else {
-            a = a*3 +1
-        }
-
-        if 1 <= a {
-            print!("{} ",a);
-        }
+    let mut tree:BTreeMap<u64,u64> = BTreeMap::new();
+    for elt in split {
+        *tree
+            .entry(elt.parse::<u64>().unwrap())
+            .or_insert(0)+=1;
     }
+    println!("tree {:?}",tree);
 }
