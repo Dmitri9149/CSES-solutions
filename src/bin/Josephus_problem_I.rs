1fafa088098b5ce1f24c2ae68cc1abb5464b9c2a
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-25 20:24:58 +0300

    some development

--- src/bin/Josephus_problem_I.rs
+++ src/bin/Josephus_problem_I.rs
@@ -13,6 +13,12 @@ fn main() {
         .unwrap()
         .expect("failed to read first line")
         .parse::<usize>().unwrap();
+
+    let range = 0..=number;
+
+
+
+
     let mut tree:BTreeMap<usize,bool>= BTreeMap::new();
     for i in 1..=number {
         tree
@@ -24,16 +30,42 @@ fn main() {
 
 // 'true'  if iterate 'to the left' , in normal way
 // 'false' if we will iterate 'to the right' , i.e. will use Rev<> iterator
-    let mut direction:bool=true;
-    let mut tree1=tree;
+    let closure_a = |mut flag|-> {
+    let mut tr:HashSet<usize>=HashSet::new();
+        for (elt,cond) in range.cycle() {
+//            println!("flag before if {}",flag);
+//            println!("elt before if {}",elt);
+                
+            if flag == true {
+                print!("{:?} ",&elt);
+                flag = false;  
+
+//                println!("flag in insert {}",flag);
+//                tree1.insert(*elt,*cond);
+//                flag=true;
+//                println!("flag in insert {}",flag);
+            } else {
+//                println!(" elt in insert {}",&elt);
+                tr.insert(*elt,*cond);
+                flag=true;
+
+//                print!("{:?} ",elt);
+//                flag = false;  
+            }
+        }
+//            println!("closure_a: {:?}",&tr);
+        tr
+
+    };
+
 /*
     let tree_b:BTreeMap<usize,bool>= BTreeMap::new();
     let mut tree_iter_b=tree_b.into_iter().rev();
     */
 //    let mut tree1:BTreeMap<usize,bool>= BTreeMap::new();
+/*
     loop {
         let closure_a = |mut flag|-> BTreeMap<usize,bool> {
-            let mut tr = BTreeMap::new();
             for (elt,cond) in tree1.iter() {
     //            println!("flag before if {}",flag);
     //            println!("elt before if {}",elt);
@@ -106,4 +138,5 @@ fn main() {
 //        print!("after match)");
 //        break;
     }
+*/
 }
