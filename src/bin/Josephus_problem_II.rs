de8831519c199f3277eb93d376c066befb5b2ea7
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-26 13:58:46 +0300

    time limit is not ok for some tests

--- src/bin/Josephus_problem_II.rs
+++ src/bin/Josephus_problem_II.rs
@@ -28,27 +28,49 @@ fn main() {
     }
     let mut elt:usize;
     let mut vect1:Vec<usize>=Vec::new();
+    let mut counter = 0;
     loop {
         let size = vect.len();
-        if size <= step{
-            for elt in vect.iter() {
-                print!("{:?} ",*elt);
+        if size >= step +1 {
+
+            let rest = size - (size / (step+1))*(step+1);
+            if rest != 0  {
+                for i in 0..step {
+                vect1 = vect[(size-rest)..size].to_owned();
+                }
             }
-            break;
-        }
-        let rest = size % (step+1);
-        if rest != 0  {
-            for i in 0..step {
-            vect1 = vect[(size-rest)..size].to_owned();
+//            println!("rest {} vect1 {:?}",&rest, &vect1);
+            let mut iter1 = vect.iter();
+            for i in 0..(size - rest) {
+                elt = *iter1.next().unwrap();
+                if (i+1)%(step+1)==0 {
+                    print!("{:?} ",elt);
+                } else {
+//                    println!("push elt {}",&elt);
+                    vect1.push(elt);
+                }
             }
-        }
-        let mut iter = vect.iter();
-        for i in 0..(size - rest) {
-            elt = *iter.next().unwrap();
-            if (i+1)%(step+1)==0 {
-                print!("{:?} ",elt);
-            } else {
-                vect1.push(elt);
+//        } else if size == step {
+//            for elt in vect.iter() {
+//                print!("{} ",elt);
+//            }
+        } else {
+            let mut quant = step%size;
+            let mut iter1 = vect.iter();
+
+            let rest = size - (quant+1); 
+            if rest != 0  {
+                for i in 0..step {
+                vect1 = vect[(size-rest)..size].to_owned();
+                }
+            }
+            for i in 0..(size-rest) {
+                elt = *iter1.next().unwrap();
+                if (i+1)%(quant+1)==0  {
+                    print!("{:?} ",elt);
+                } else {
+                    vect1.push(elt);
+                }
             }
         }
         if vect1.len() == 0 {
@@ -56,5 +78,10 @@ fn main() {
         }
         vect=vect1;
         vect1=vec![];
+/*
+        println!("vect: {:?}", &vect);
+        counter+=1;
+        if counter == 8 {break;}
+*/
     }
 }
