3bcc76b7aa0461b4ec4f136ac2c6c7b0254ae8f7
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-27 12:06:00 +0300

    some changes

--- src/bin/Josephus_problem_II.rs
+++ src/bin/Josephus_problem_II.rs
@@ -3,6 +3,19 @@ use std::io;
 use std::str::SplitWhitespace;
 use std::convert::TryInto;
 
+pub fn rec(size:u32,step:u32) -> u32 {
+    let mut new:u32;
+
+    if size == 1 {
+    print!("{} ",1);
+        return 1;
+    }
+
+    let new = (rec(size-1, step) + step-1) % size +1;
+    print!("{} ",new);
+    return(new);
+}
+
 fn main() {
     let stdin = io::stdin();
     let mut iter_line = stdin.lock().lines();
@@ -22,68 +35,5 @@ fn main() {
         .unwrap()
         .parse::<u32>()
         .unwrap();
-    let mut vect:Vec<u32>=(1..=total).collect();
-    let mut iter1;
-    let mut elt:u32;
-    let mut vect1;;
-    let mut quant:u32;
-    let mut size:u32;
-    let mut delta;
-    loop {
-        size = vect.len() as u32;
-        if size >= (step +1) as u32 {
-
-<<<<<<< HEAD
-/*
-            let rest = size - (size / (step+1))*(step+1);
-//            if rest != 0  {
-                vect1 = vect[(size-rest)..size].to_owned();
-//            }
-//            println!("rest {} vect1 {:?}",&rest, &vect1);
-*/
-
-            let new_point = (size/(step+1))*(step+1);
-            vect1 = vect[new_point..size].to_owned();
-
-            let mut iter1 = vect.iter();
-            for i in 0..new_point {
-=======
-            delta = (size / (step+1))*(step+1);
-                vect1 = vect[delta as usize..size as usize].to_owned();
-            iter1 = vect.iter();
-            for i in 0..delta {
->>>>>>> j_p_u32
-                elt = *iter1.next().unwrap();
-                if (i+1)%(step+1)==0 {
-                    print!("{:?} ",elt);
-                } else {
-                    vect1.push(elt);
-                }
-            }
-        } else if size == (step +1) as u32 {
-            print!("{} ",&vect.pop().unwrap());
-            vect1=vect;
-        } else {
-            quant = step -(step/size)*size;
-            if quant == 0 {
-                print!("{} ",&vect[0]);
-                vect1=vect[1..].to_vec();
-            } else {
-
-                iter1 = vect.iter();
-                    vect1 = vect[(quant+1) as usize ..size as usize].to_owned(); 
-                for i in 0..quant {
-                    elt = *iter1.next().unwrap();
-                    vect1.push(elt);
-                }
-                elt = *iter1.next().unwrap();
-                print!("{} ",elt);            
-
-            }
-        }
-        if vect1.len() == 0 {
-            break;
-        }
-        vect=vect1;
-    }
+    rec(total,step);
 }
