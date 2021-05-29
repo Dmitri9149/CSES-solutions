251865c32718b8540d218afce9ded8f6883e1e81
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-29 13:40:27 +0300

    initial commit

---
+++ src/bin/removing_game.rs
@@ -0,0 +1,46 @@
+use std::io::{BufRead};
+use std::io;
+use std::str::SplitWhitespace;
+
+pub struct Tensor2D {
+    tensor:Vec<Vec<32>,
+}
+impl Tensor2D {
+    pub fn new(list:&Vec<u32>) -> {
+        Tensor2D {
+            tensor:Vec::with_capacity(n)
+        }
+    }
+}
+pub fn read_lines() -> Vec<u64> {
+    let stdin = io::stdin();
+    let iter:SplitWhitespace;
+    let mut iter_line = stdin.lock().lines();
+    let number = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read first line")
+        .parse::<u64>().unwrap();
+    let mut vect:Vec<u64>= Vec::new();
+    let line = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read second line");
+    iter = line.split_whitespace();
+    for elt in iter {
+        vect.push(elt.parse::<u64>().unwrap());
+    }
+    vect
+}
+
+fn main() {
+    let mut vect = read_lines();
+    vect.sort();
+    let mut current_sum =0;
+    for elt in vect.iter() {
+        if elt > &(current_sum +1) {
+            break;
+        } else {current_sum +=elt;}
+    }
+    println!("{}",current_sum+1);
+}
