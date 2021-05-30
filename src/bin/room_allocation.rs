0416b77c42eb577c8f92bb44a03e8ef75395e3a3
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-30 17:36:21 +0300

    read data is ok

--- src/bin/room_allocation.rs
+++ src/bin/room_allocation.rs
@@ -1,22 +1,12 @@
-/*
-In a movie festival n movies will be shown. You know the starting and ending time of each movie. What is the maximum number of movies you can watch entirely?
-
-Input
-
-The first input line has an integer n: the number of movies.
-
-After this, there are n lines that describe the movies. Each line has two integers a and b: the starting and ending times of a movie.
-
-Output
-
-Print one integer: the maximum number of movies.
-*/
+// https://cses.fi/alon/task/1164
+//
 use std::io::{BufRead};
 use std::collections::BTreeMap;
 use std::io;
 use std::str::SplitWhitespace;
+use std::convert::TryInto;
 
-pub fn read_lines() -> BTreeMap<u64,u64> {
+pub fn read_lines() -> Vec<((u32,u32),u32)> {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
@@ -24,38 +14,27 @@ pub fn read_lines() -> BTreeMap<u64,u64> {
         .next()
         .unwrap()
         .expect("failed to read next line")
-        .parse::<u64>().unwrap();
-    let mut tree:BTreeMap<u64,u64> = BTreeMap::new();
-    let mut seed:(u64,u64)=(0,0);
+        .parse::<u32>().unwrap();
+    let mut data:Vec<((u32,u32),u32)> = Vec::with_capacity(number.try_into().unwrap());
+    let mut seed:(u32,u32)=(0,0);
+    let mut index = 0;
     for line in iter_line {
         let input = line.expect("Failed to read line");
         iter = input.split_whitespace();
-        seed.0 = iter.next().unwrap().parse::<u64>().unwrap();
-        seed.1 = iter.next().unwrap().parse::<u64>().unwrap();
-        match tree.get_mut(&seed.1) {
-            Some(x) => if seed.0 > *x {*x=seed.0},
-            None => {
-                tree.insert(seed.1,seed.0);
-            }
-        }
-        number-=1;
-        if number == 0 {
+        seed.0 = iter.next().unwrap().parse::<u32>().unwrap();
+        seed.1 = iter.next().unwrap().parse::<u32>().unwrap();
+        data.push(((seed.0,seed.1),index));
+        index+=1;
+        if index  == number {
             break;
         }
     }
-    tree
+    data
 }
 
 fn main() {
-    let tree = read_lines();
-    let mut count=0;
-    let mut end = 0;
-    for (fst, snd) in tree.iter() {
-        if snd >= &end {
-            count+=1;
-            end = *fst;
-        }
-    }
-    println!("{}",count);
+    let mut data = read_lines();
+    data.sort_by(|a, b| a.0.0.partial_cmp(&b.0.0).unwrap());
+    ()
 }
 
