112222a3763c2d5cdca94abe4f57808f20ca36bf
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-31 15:00:03 +0300

    read data

--- src/bin/subarray_distinct_values.rs
+++ src/bin/subarray_distinct_values.rs
@@ -5,62 +5,38 @@ use std::collections::BTreeMap;
 use std::io;
 use std::str::SplitWhitespace;
 
-pub fn read_lines() -> (Vec<(usize,usize)>,Vec<(usize,usize)>,usize) {
+pub fn read_lines() -> (usize,usize,Vec<usize>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
-    let number = iter_line
+    let integers = iter_line
         .next()
         .unwrap()
-        .expect("failed to read next line")
+        .expect("failed to read first line")
         .parse::<usize>().unwrap();
-    let mut start:Vec<(usize,usize)> = Vec::with_capacity(number);
-    let mut end:Vec<(usize,usize)> = Vec::with_capacity(number);
-    let mut seed:(usize,usize)=(0,0);
+    let subarrays = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read second line")
+        .parse::<usize>().unwrap();
+
+    let mut collection:Vec<usize> = Vec::with_capacity(integers);
     let mut index = 0;
+    let mut seed = 0;
     for line in iter_line {
-        let input = line.expect("Failed to read line");
+        let input = line.expect("Failed to last line");
         iter = input.split_whitespace();
-        seed.0 = iter.next().unwrap().parse::<usize>().unwrap();
-        seed.1 = iter.next().unwrap().parse::<usize>().unwrap();
-        start.push((seed.0,index));
-        end.push((seed.1,index));
+        seed = iter.next().unwrap().parse::<usize>().unwrap();
+        collection.push(seed);
         index+=1;
-        if index  == number {
+        if index  == integers {
             break;
         }
     }
-    (start,end,number)
+    (integers, subarrays,collection)
 }
 
 fn main() {
-    let (mut input, mut output, num) = read_lines();
-    let mut res:Vec<usize> = Vec::with_capacity(num);
-    for _i in 0..num {
-        res.push(0);
-    }
-
-    let mut free:BTreeMap<usize,bool> = BTreeMap::new();
-    for i in 0..num {
-        free.insert(i,true);
-    }
-    input.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-    output.sort_by(|a, b| a.0.partial_cmp(&b.0).unwrap());
-    let mut k;
-    let mut j =0;
-    for i in 0..num {
-        while j < num && output[j].0 < input[i].0 {
-            free.insert(res[output[j].1],true);
-            j+=1;
-        }
-        k = *free.iter().next().unwrap().0;
-        res[input[i].1] = k;
-        free.remove(&k);    
-    }
-    let max = res.iter().max().unwrap() +1;
-    println!("{}",&max);
-    for elt in res.iter() {
-        print!("{} ",elt + 1 );
-    }
+    let (integers, subarrays,collection) = read_lines();
 }
 
