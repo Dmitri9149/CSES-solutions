311c4746403fa7ce783483a334429204400ef397
dmitri9149 <dmitri.as.name@gmail.com>
2021-05-31 17:43:45 +0300

    some development, but the task was understood wrongly, have to remake

--- src/bin/subarray_distinct_values.rs
+++ src/bin/subarray_distinct_values.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead};
 use std::collections::BTreeMap;
 use std::io;
 use std::str::SplitWhitespace;
+use std::collections::BTreeSet;
 
 fn powerset<T>(s: &[T]) -> Vec<Vec<&T>> {
     (0..2usize.pow(s.len() as u32)).map(|i| {
@@ -16,6 +17,23 @@ pub fn read_lines() -> (usize,usize,Vec<usize>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
+    let mut first_line = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read first line");
+    let mut fst_line_iter = first_line.split_whitespace();
+    let integers = fst_line_iter
+        .next()
+        .unwrap()
+        .parse::<usize>().unwrap();
+    println!("integers {}", &integers);
+    let subarrays = fst_line_iter
+        .next()
+        .unwrap()
+        .parse::<usize>().unwrap();
+    println!("subarrays {}", &subarrays);
+
+/*
     let integers = iter_line
         .next()
         .unwrap()
@@ -26,29 +44,39 @@ pub fn read_lines() -> (usize,usize,Vec<usize>) {
         .unwrap()
         .expect("failed to read second line")
         .parse::<usize>().unwrap();
-
+*/
     let mut collection:Vec<usize> = Vec::with_capacity(integers);
     let mut index = 0;
     let mut seed = 0;
     for line in iter_line {
         let input = line.expect("Failed to last line");
         iter = input.split_whitespace();
-        seed = iter.next().unwrap().parse::<usize>().unwrap();
-        collection.push(seed);
-        index+=1;
-        if index  == integers {
-            break;
+        for i in 0..integers {
+            seed = iter.next().unwrap().parse::<usize>().unwrap();
+            println!("index {} and seed {}", &index, &seed);
+            collection.push(seed);
         }
+        break;
     }
     (integers, subarrays,collection)
 }
 
 fn main() {
     let (integers, subarrays,collection) = read_lines();
-    for elt in (0..2usize.pow(integers as u32)).map(|i| {
+    let mut check:BTreeSet<Vec<&usize>>=BTreeSet::new();
+    println!(" {}  {}  {:?}", &integers,&subarrays,&collection);
+    let mut counter =0;
+    for mut elt in (0..2usize.pow(integers as u32)).map(|i| {
          collection.iter().enumerate().filter(|&(t, _)| (i >> t) % 2 == 1)
              .map(|(_, element)| element).collect::<Vec<&usize>>()}) {    
-        println!("{:?}",elt);
+        let elt_original = elt.to_owned();
+        elt.sort();
+        elt.dedup();
+        if elt.len() <= subarrays && check.insert(elt_original.to_owned()) {
+            println!("{:?}",&elt_original);
+            counter+=1;
+        }
     }
+    println!("counter {}",counter);
 }
 
