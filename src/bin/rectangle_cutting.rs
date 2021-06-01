cee803d73b6f787982a7aed6fc4e7973382fc48c
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-01 18:06:56 +0300

    tests are passed, but will try to improove the time

--- src/bin/rectangle_cutting.rs
+++ src/bin/rectangle_cutting.rs
@@ -3,6 +3,7 @@ use std::io::{BufRead};
 use std::io;
 use std::str::SplitWhitespace;
 use std::collections::HashMap;
+use std::cmp;
 
 pub fn read_lines() -> (usize,usize) {
     let stdin = io::stdin();
@@ -27,9 +28,34 @@ pub fn read_lines() -> (usize,usize) {
 
 fn main() {
     let (aa, bb) = read_lines();
+/*
     let mut count:usize=0;
     let mut left:usize=0;
     let mut right:usize=0;
-    println!("{}",aa);
+*/
+    let mut res:Vec<Vec<usize>>= Vec::with_capacity(aa+1);
+    let mut internal:Vec<usize>=Vec::with_capacity(bb+1);
+    for i in 0..=bb {
+        internal.push(std::u32::MAX as usize);
+    }
+    for i in 0..=aa {
+        res.push(internal.to_owned());
+    }
+    for  i in 0..=aa {
+        for  j in 0..=bb {
+            if i == j {
+                res[i][j] = 0;
+            } else {
+//                res[i][j] = usize::MAX;
+                for  k in 1..i {
+                    res[i][j] = cmp::min(res[i][j], res[k][j]+res[i-k][j]+1);
+                }
+                for  k in 1..j {
+                    res[i][j] = cmp::min(res[i][j], res[i][k]+res[i][j-k]+1);
+                }
+            }
+        }
+    }
+    println!("{}",res[aa][bb]);
 }
 
