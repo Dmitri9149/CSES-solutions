7b8f1a20696a04e211555e5de8466ffe84807a3a
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-01 11:54:28 +0300

    all is working, some small changes are to be done

--- src/bin/subarray_distinct_values.rs
+++ src/bin/subarray_distinct_values.rs
@@ -6,14 +6,6 @@ use std::str::SplitWhitespace;
 use std::collections::BTreeSet;
 use std::collections::HashSet;
 
-fn powerset<T>(s: &[T]) -> Vec<Vec<&T>> {
-    (0..2usize.pow(s.len() as u32)).map(|i| {
-         s.iter().enumerate().filter(|&(t, _)| (i >> t) % 2 == 1)
-                             .map(|(_, element)| element)
-                             .collect()
-     }).collect()
-}
-
 pub fn read_lines() -> (usize,usize,Vec<usize>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
@@ -27,22 +19,18 @@ pub fn read_lines() -> (usize,usize,Vec<usize>) {
         .next()
         .unwrap()
         .parse::<usize>().unwrap();
-//    println!("integers {}", &integers);
     let subarrays = fst_line_iter
         .next()
         .unwrap()
         .parse::<usize>().unwrap();
-//    println!("subarrays {}", &subarrays);
 
     let mut collection:Vec<usize> = Vec::with_capacity(integers);
-    let mut index = 0;
     let mut seed = 0;
     for line in iter_line {
         let input = line.expect("Failed to last line");
         iter = input.split_whitespace();
         for i in 0..integers {
             seed = iter.next().unwrap().parse::<usize>().unwrap();
-//            println!("index {} and seed {}", &index, &seed);
             collection.push(seed);
         }
         break;
@@ -52,27 +40,28 @@ pub fn read_lines() -> (usize,usize,Vec<usize>) {
 
 fn main() {
     let (integers, subarrays,collection) = read_lines();
-    let mut counter:usize=0;
-    let mut temp:HashSet<Vec<usize>>=HashSet::with_capacity(integers);
-    for i in 0..integers {
-    let mut check:HashSet<usize>=HashSet::with_capacity(subarrays +1);
-    let mut temp_vec:Vec<usize>=vec![];
-        for j  in  i..integers {
-            temp_vec.push(collection[j]);
-            check.insert(collection[j]);
-            if check.len() <= subarrays {
-                if temp.insert(temp_vec.to_owned()) {
-                    counter+=1;
-                    println!("i {} and j {}", i, j);
-//                    println!("check: {:?}", check); 
-                  println!("counts {}",counter);
-                    println!("temp {:?}",temp);
-                }
+    let mut count:usize=0;
+    let mut left:usize=0;
+    let mut right:usize=0;
+    let mut map:BTreeMap<usize,usize>= BTreeMap::new();
+    while right < integers {
+        *map
+            .entry(collection[right])
+            .or_insert(0)+=1;
+        while map.keys().len() > subarrays {
+            if *map.get(&collection[left]).unwrap() == 1 {
+                map.remove(&collection[left]); 
             } else {
-                break;
-            }          
-        }   
+                *map
+                .entry(collection[left])
+                .or_insert(collection[left])-=1;
+            }
+
+            left +=1;
+        }
+        count +=right - left +1;
+        right+=1;
     }
-    println!("{}",counter+1);
+    println!("{}",count);
 }
 
