500f1c69b258991695ebb15dc4d1ba4d535bbaa6
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-02 12:31:58 +0300

    change to arrays, Time Result is better

--- src/bin/rectangle_cutting.rs
+++ src/bin/rectangle_cutting.rs
@@ -5,7 +5,7 @@ use std::str::SplitWhitespace;
 use std::collections::HashMap;
 use std::cmp;
 
-pub fn read_lines() -> (usize,usize) {
+pub fn read_lines() -> (u16,u16) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
@@ -17,39 +17,61 @@ pub fn read_lines() -> (usize,usize) {
     let aa = fst_line_iter
         .next()
         .unwrap()
-        .parse::<usize>().unwrap();
+        .parse::<u16>().unwrap();
     let bb = fst_line_iter
         .next()
         .unwrap()
-        .parse::<usize>().unwrap();
+        .parse::<u16>().unwrap();
 
     (aa, bb)
 }
+/*
+pub fn mins(size: u16) -> Vec<u16> {
+    vec![1000000000; size as usize]
+}
+*/
 
 fn main() {
     let (aa, bb) = read_lines();
-    let mut res:Vec<Vec<usize>>= Vec::with_capacity(aa+1);
-    let mut internal:Vec<usize>=Vec::with_capacity(bb+1);
+    let mut res = [[501; 501];501];
+//    let mut res:Vec<Vec<u32>>= Vec::with_capacity((aa+1) as usize);
+//    let mut internal:Vec<u32>=Vec::with_capacity((bb+1) as usize);
+/*
     for i in 0..=bb {
-        internal.push(std::u32::MAX as usize);
+        internal.push(1000000000);
     }
+*/
+/*
+    internal=mins(bb+1);
+*/
+    if aa==bb {
+        println!("{}",0);
+        return ();
+    }
+/*
     for i in 0..=aa {
         res.push(internal.to_owned());
     }
+//i    internals=mins(bb+1);
+*/
     for  i in 0..=aa {
         for  j in 0..=bb {
             if i == j {
-                res[i][j] = 0;
+                res[i as usize ][j as usize ] = 0;
             } else {
 //                res[i][j] = usize::MAX;
                 for  k in 1..i {
-                    res[i][j] = cmp::min(res[i][j], res[k][j]+res[i-k][j]+1);
+                    res[i as usize][j as usize] = cmp
+                        ::min(res[i as usize][j as usize]
+                              , res[k as usize][j as usize]+res[(i-k) as usize][j as usize]+1);
                 }
                 for  k in 1..j {
-                    res[i][j] = cmp::min(res[i][j], res[i][k]+res[i][j-k]+1);
+                    res[i as usize ][j as usize] = cmp
+                        ::min(res[i as usize][j as usize]
+                              , res[i as usize][k as usize]+res[i as usize][(j-k) as usize]+1);
                 }
             }
         }
     }
-    println!("{}",res[aa][bb]);
+    println!("{}",res[aa as usize ][bb as usize]);
 }
