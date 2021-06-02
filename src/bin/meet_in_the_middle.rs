a169f5f3e693921472735aafe4c261dddd7e974e
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-02 16:52:26 +0300

    initial commit

---
+++ src/bin/meet_in_the_middle.rs
@@ -0,0 +1,64 @@
+//https://cses.fi/alon/task/2428
+use std::io::{BufRead};
+use std::io;
+use std::str::SplitWhitespace;
+use std::collections::HashMap;
+
+pub fn read_lines() -> (usize,usize,Vec<u32>) {
+    let stdin = io::stdin();
+    let mut iter:SplitWhitespace; 
+    let mut iter_line = stdin.lock().lines();
+    let first_line = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read first line");
+    let mut fst_line_iter = first_line.split_whitespace();
+    let integers = fst_line_iter
+        .next()
+        .unwrap()
+        .parse::<usize>().unwrap();
+    let subarrays = fst_line_iter
+        .next()
+        .unwrap()
+        .parse::<usize>().unwrap();
+
+    let mut collection:Vec<u32> = Vec::with_capacity(integers);
+    let mut seed = 0;
+    for line in iter_line {
+        let input = line.expect("Failed to last line");
+        iter = input.split_whitespace();
+        for _i in 0..integers {
+            seed = iter.next().unwrap().parse::<u32>().unwrap();
+            collection.push(seed);
+        }
+        break;
+    }
+    (integers, subarrays,collection)
+}
+
+fn powerset<T>(s: &[T]) -> Vec<Vec<&T>> {
+    (0..2usize.pow(s.len() as u32)).map(|i| {
+         s.iter().enumerate().filter(|&(t, _)| (i >> t) % 2 == 1)
+                             .map(|(_, element)| element)
+                             .collect()
+     }).collect()
+}
+
+fn summ(s: &[u32]) -> Vec<u32> {
+    (0..2usize.pow(s.len() as u32)).map(|i| {
+         s.iter().enumerate().filter(|&(t, _)| (i >> t) % 2 == 1)
+                             .map(|(_, element)| *element).sum()
+//                             .collect::<Vec<usize>>().sum()
+
+     }).collect()
+}
+fn main() {
+    let (integers, subarrays,collection) = read_lines();
+    let mut count:usize=0;
+    let mut left:usize=0;
+    let mut right:usize=0;
+    let mut sums:Vec<u32>= Vec::with_capacity(integers);
+    sums = summ(&collection);
+    println!("{:?}",sums);
+}
+
