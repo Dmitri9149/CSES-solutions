3c97baa69ee97cc3aa80ae5f47942769dcea8947
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-04 14:08:05 +0300

    all tests are passed

---
+++ src/bin/two_sets_II.rs
@@ -0,0 +1,39 @@
+//https://cses.fi/alon/result/2273368/
+use std::io::{BufRead};
+use std::{io,cmp};
+
+//some ideas from here https://www.youtube.com/watch?v=TOsD3BkIKoQ  (in c++)
+const modul:usize = 1000000000 +7;
+fn main() {
+    let stdin = io::stdin();
+    let mut iter_line = stdin.lock().lines();
+    let number = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read first line")
+        .parse::<usize>().unwrap();
+    let mut res:Vec<Vec<usize>>=Vec::with_capacity(number+1);
+    let size = cmp::max(2,number*number);
+    let mut interm:Vec<usize>=Vec::with_capacity(size/2);
+    for i in 0..size/2 {
+        interm.push(0);
+    }
+    for i in 0..number+1 {
+        res.push(interm.to_owned());
+    }
+    res[0][0]=1;
+    for i in 1..=number {
+        for x in 0..=(number*(number+1))/4 {
+            if x < i {
+                res[i][x]=res[i-1][x]%modul;
+            } else {
+                res[i][x]=(res[i-1][x-i] + res[i-1][x])%modul;
+            }
+        }   
+    }
+    if (number*(number+1)%4) != 0 {
+        print!("{}",0);
+    } else {
+        print!("{}",(res[number][number*(number+1)/4]*500000004)%1000000007);
+    }
+}
