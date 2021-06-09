dc98792320ee2e62cf040e3afcf7a48db21ff58c
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-09 19:03:20 +0300

    some final changes, all is working

--- src/bin/counting_towers.rs
+++ src/bin/counting_towers.rs
@@ -1,34 +1,16 @@
 use std::io::{BufRead};
-use std::collections::HashMap;
 use std::io;
 const MOD:usize = 1000000000 +7;
+const SIZE:usize = 1000000;
 
-
-pub fn how_many(n:&usize) -> ([usize;1000002],[usize;1000002]){
-    let mut dp_0 = [0;1000002];
-    let mut dp_1 = [0;1000002];
-    dp_0[1]=1;
-    dp_1[1]=1;
-    dp_0[2]=3;
-    dp_1[2]=5;
-    let mut ins_0;
-    let mut ins_1;
-    for i in 3..=*n {
-        ins_0 = (dp_0[i-1]*2 + dp_1[i-1])%MOD;
-        ins_1 = (dp_1[i-1]*4+dp_0[i-1])%MOD;
-        dp_0[i]=ins_0;
-        dp_1[i]=ins_1;
-    }
-    (dp_0,dp_1)
-}
 fn main() {
-    let mut dp_0 = [0;1000002];
-    let mut dp_1 = [0;1000002];
+    let mut dp_0 = [0;SIZE+1];
+    let mut dp_1 = [0;SIZE+1];
     dp_0[1]=1;
     dp_1[1]=1;
     dp_0[2]=3;
     dp_1[2]=5;
-    for i in 3..=1000000 {
+    for i in 3..=SIZE {
         dp_0[i]=(dp_0[i-1]*2 + dp_1[i-1])%MOD;
         dp_1[i]=(dp_1[i-1]*4+dp_0[i-1])%MOD;
     }
