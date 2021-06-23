8b9db57537f9c9b2e3b6e14794eccacf8d9c24ae
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-23 17:05:07 +0300

    initial commit

---
+++ src/bin/nearest_smaller_values.rs
@@ -0,0 +1,35 @@
+//https://cses.fi/alon/task/1074
+use std::io::{BufRead};
+use std::io;
+use std::str::SplitWhitespace;
+ 
+pub fn read_lines() -> (usize,Vec<u32>) {
+    let stdin = io::stdin();
+    let iter:SplitWhitespace;
+    let mut iter_line = stdin.lock().lines();
+    let number = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read first line")
+        .parse::<usize>().unwrap();
+    let mut vect:Vec<u32>= Vec::with_capacity(number);
+    let line = iter_line
+        .next()
+        .unwrap()
+        .expect("failed to read second line");
+    iter = line.split_whitespace();
+    for elt in iter {
+        vect.push(elt.parse::<u32>().unwrap());
+    }
+
+    if number != vect.len() {
+        panic!("declared lenght and real lengths are different, panic!");
+    }
+
+    (number,vect)
+}
+ 
+fn main() {
+    let (size,mut data) = read_lines();
+    print!("{:?}  {:?}",size,data);
+}
