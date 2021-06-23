1a7e435c29590fc12165adc400e2164229039684
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-23 13:43:25 +0300

    some simplifications

--- src/bin/counting_numbers.rs
+++ src/bin/counting_numbers.rs
@@ -24,48 +24,50 @@ pub fn read_lines() -> (usize,usize) {
 
 pub struct Dp {
    pub  dp:[[[[Option<usize>;19];10];2];2],
-   pub num:Vec<usize>,
 }
 impl Dp {
     pub fn new() -> Dp {
         Dp {
             dp:[[[[None;19];10];2];2],
-            num:Vec::with_capacity(19),
         }
     }
-    pub fn num(&mut self, mut n:usize) {
-        while n > 0 {
-            self.num.push(n % 10);
-            n /= 10;
-        }
-        self.num.reverse();
-    }
-    pub fn calc(&mut self,pos:usize,dig:usize,zeros:usize,range:usize) -> Option<usize> {
-        if pos == self.num.len() {return Some(1)};
+    pub fn calc(&mut self,pos:usize,dig:usize,zeros:usize,range:usize,num:&Vec<usize>) 
+        -> Option<usize> {
+        if pos == num.len() {return Some(1)};
         if self.dp[range][zeros][dig][pos] != None {return self.dp[range][zeros][dig][pos]};
         let mut res = 0;
-        let lmt = if zeros == 1 {9} else {self.num[pos]};
+        let lmt = if zeros == 1 {9} else {num[pos]};
         let mut handle_zeros;
         let mut handle_range;
         for i in 0..=lmt {
             handle_zeros = if zeros == 1 || (i < lmt) {1} else {0};
             handle_range = if range == 1 || (i > 0) {1} else {0};
             if (i != dig || (range == 0  && i == 0)) {
-                res += self.calc(pos +1, i,handle_zeros,handle_range).unwrap();
+                res += self.calc(pos +1, i,handle_zeros,handle_range,num).unwrap();
             }
         }
         self.dp[range][zeros][dig][pos] = Some(res);
         return Some(res);
     }
 }
+pub fn num(n:usize) -> Vec<usize> {
+    let mut nn = n;
+    let mut num:Vec<usize> = Vec::with_capacity(19);
+    while nn > 0 {
+        num.push(nn % 10);
+        nn /= 10;
+    }
+    num.reverse();
+    num
+}
 
 fn main() {
     let (mut aa, bb) = read_lines();
     let mut closure = |n| -> usize {
+        let num = num(n);
         let mut dp_xx = Dp::new();
         if n == 0 {return 1};
-        dp_xx.num(n);
-        dp_xx.calc(0,0,0,0).unwrap()
+        dp_xx.calc(0,0,0,0,&num).unwrap()
     };
     if aa == 0 {
         print!("{}", closure(bb));
