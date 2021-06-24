23292500cab4bb7b3445806b5d7955d87e72fa2b
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-24 13:26:03 +0300

    some bugs are fixed

--- src/bin/factory_machines.rs
+++ src/bin/factory_machines.rs
@@ -3,7 +3,7 @@ use std::io;
 use std::str::SplitWhitespace;
 use std::collections::HashMap;
 
-pub fn read_lines() -> (usize,usize,Vec<u64>) {
+pub fn read_lines() -> (usize,usize,Vec<usize>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
@@ -21,13 +21,13 @@ pub fn read_lines() -> (usize,usize,Vec<u64>) {
         .unwrap()
         .parse::<usize>().unwrap();
 
-    let mut collection:Vec<u64> = Vec::with_capacity(machines);
+    let mut collection:Vec<usize> = Vec::with_capacity(machines);
     let mut seed = 0;
     for line in iter_line {
         let input = line.expect("Failed to last line");
         iter = input.split_whitespace();
         for _i in 0..machines {
-            seed = iter.next().unwrap().parse::<u64>().unwrap();
+            seed = iter.next().unwrap().parse::<usize>().unwrap();
             collection.push(seed);
         }
         break;
@@ -38,21 +38,22 @@ pub fn read_lines() -> (usize,usize,Vec<u64>) {
 fn main() {
     let (machines, products,collection) = read_lines();
     let mut low = 0;
-    let mut high:u64 = 1000000000000000000;
-    let mut res:u64 = 1000000000000000000;
+    let mut high:usize = 1000000000000000000;
+    let mut res:usize = 1000000000000000000;
     while low <= high {
 //        println!("mid: {}  prod: {}  low: {  } high: {}   res: {}",mid,prod,low,high,res);
         let mut mid;
         let mut prod = 0;
         mid = (low + high) / 2;
         for (i,time)  in collection.iter().enumerate() {
-            prod += std::cmp::min(mid as u64/time,1000000000);
+            prod += std::cmp::min(mid/time,1000000000);
         }
-        if prod >= products as u64 {
+        if prod >= products {
             if mid < res {
                 res = mid;
-                high = mid -1;         
+//                high = mid -1;         
             }
+            high = mid -1;
         } else {
             low = mid + 1;
         } 
