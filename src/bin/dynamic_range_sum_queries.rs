f26a5eef51f375e0cf92a814a5299d2e37fa5d38
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-27 20:00:08 +0300

    some unfinished development, will not merge to main

--- src/bin/dynamic_range_sum_queries.rs
+++ src/bin/dynamic_range_sum_queries.rs
@@ -5,7 +5,7 @@ use std::collections::HashMap;
 
 //const MAXSIZE:usize= 2*2_usize.pow(200000f64.log2().ceil() as u32);
 const MAXSIZE:usize = 524288 -1; // calculated from the 200000 array size
-pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
+pub fn read_lines() -> (usize,usize,[usize;200000],HashMap<usize,[usize;3]>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
@@ -23,7 +23,8 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
         .unwrap()
         .parse::<usize>().unwrap();
 
-    let mut collection:Vec<usize> = Vec::with_capacity(values);
+//    let mut collection:Vec<usize> = Vec::with_capacity(values);
+    let mut collection:[usize;200000] = [0;200000];
     let mut seed;
 /*
     for line in iter_line {
@@ -38,9 +39,10 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
 */
     let second_input = iter_line.next().unwrap().expect("Failed to read seconf line");
     iter = second_input.split_whitespace();
-    for _i in 0..values {
+    for i in 0..values {
         seed = iter.next().unwrap().parse::<usize>().unwrap();
-        collection.push(seed);
+//        collection.push(seed);
+        collection[i]=seed;
     }
     let mut lines_number = 0;
     let mut qrs:HashMap<usize,[usize;3]> = HashMap::with_capacity(queries);
@@ -60,38 +62,39 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
 
     (values,queries,collection,qrs)
 }
-pub fn constructMidNode(s:usize,e:usize) -> usize {
+pub fn construct_mid_node(s:usize,e:usize) -> usize {
     s + (e-s) / 2 
 }
 
 pub struct SegmentTree {
     pub tree:[usize;MAXSIZE],
+    pub array:[usize;200000],
 }
 
 impl SegmentTree {
-    pub fn new() -> SegmentTree {
+    pub fn new(array:[usize;200000]) -> SegmentTree {
         SegmentTree {
             tree:[0;MAXSIZE],
+            array:array
         }
     }
-    pub fn from_array(&mut self,arr:&[usize;200000],start:usize,end:usize,current_node:usize) -> usize {
+    pub fn from_array(&mut self,start:usize,end:usize,current_node:usize) -> usize {
         let mut res;
         if start == end {
-            res = arr[start];
+            res = self.array[start];
             self.tree[current_node]=res;
             return res
         }
-
-        let mid = constructMidNode(start,end);
-        res = self.from_array(arr,start,mid,current_node*2+1) + 
-            self.from_array(arr, mid+1,end,current_node*2 + 1);
+        let mid = construct_mid_node(start,end);
+        res = self.from_array(start,mid,current_node*2+1) + 
+            self.from_array(mid+1,end,current_node*2 + 1);
             self.tree[current_node]=res;
             return res
     }
-    pub fn construct_tree(&mut self,arr:&[usize;200000]) {
-        self.from_array(arr,0,200000 - 1, 0);
+    pub fn construct_tree(&mut self) {
+        self.from_array(0,200000 - 1, 0);
     }
-    pub fn getSumHelper(&mut self,start:usize, end:usize
+    pub fn get_sum_helper(&mut self,start:usize, end:usize
                         ,qstart:usize,qend:usize,current_node:usize) 
         -> usize {
             if qstart <= start && qend >= end {
@@ -99,15 +102,42 @@ impl SegmentTree {
             } else if end < qstart || start > qend {
                 return 0;
             }
-            let mid = constructMidNode(start,end);
-            return self.getSumHelper(start, mid, qstart,qend,2*current_node +1) + 
-                self.getSumHelper(mid+1,end,qstart,qend,2*current_node +2);
+            let mid = construct_mid_node(start,end);
+            return self.get_sum_helper(start, mid, qstart,qend,2*current_node +1) + 
+                self.get_sum_helper(mid+1,end,qstart,qend,2*current_node +2);
+    }
+    pub fn update_value_helper(&mut self, start:usize
+                             ,end:usize, index:usize, diff:usize, current_node:usize) {
+        if index < start || index > end {
+            return ();
+        }
+        self.tree[current_node] += diff;
+        if end != start {
+            let mid = construct_mid_node(start,end);
+            self.update_value_helper(start,mid,index,diff,2*current_node +1);
+            self.update_value_helper(mid+1,end,index,diff,2*current_node +2);
+        }
+    }
+    pub fn update_value(&mut self, values:usize, index:usize, new_value:usize) {
+        if index < 0 || index > values -1 {
+            panic!("Wrong index");
+        }
+        let diff = new_value - self.array[index];
+        self.array[index]= new_value;
+        self.update_value_helper(0,values-1,index,diff,0);
+    }
+    pub fn get_sum(&mut self,values:usize,qstart:usize,qend:usize) -> usize {
+        if qstart < 0 || qend > values -1 || qstart > qend {
+            panic!("Wrong start or end index");
+        }
+
+        return self.get_sum_helper(0,values-1,qstart,qend,0);
     }
 }
-
-
 fn main() {
     let (values,queries,collection,qrs) = read_lines();
+    let mut segment_tree = SegmentTree::new(collection);
+    segment_tree.construct_tree();
     println!("{} {} {:?} {:?}",values,queries,collection,qrs);
 }
 
