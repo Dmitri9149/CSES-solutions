1a5319d6c7adedec2179abd04306e8ad8bdd2728
dmitri9149 <dmitri.as.name@gmail.com>
2021-06-28 17:37:54 +0300

    working but TLE on some tasks, alg is O(n);

--- src/bin/dynamic_range_sum_queries.rs
+++ src/bin/dynamic_range_sum_queries.rs
@@ -2,10 +2,11 @@ use std::io::{BufRead};
 use std::io;
 use std::str::SplitWhitespace;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 //use segment_tree::SegmentPoint;
 //use segment_tree::ops::Add;
 
-pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
+pub fn read_lines() -> (usize,usize,HashMap<usize,usize>,HashMap<usize,[usize;3]>) {
     let stdin = io::stdin();
     let mut iter:SplitWhitespace; 
     let mut iter_line = stdin.lock().lines();
@@ -23,7 +24,7 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
         .unwrap()
         .parse::<usize>().unwrap();
 
-    let mut collection:Vec<usize> = Vec::with_capacity(values);
+    let mut collection:HashMap<usize,usize> = HashMap::with_capacity(values);
     let mut seed;
 /*
     for line in iter_line {
@@ -38,9 +39,10 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
 */
     let second_input = iter_line.next().unwrap().expect("Failed to read seconf line");
     iter = second_input.split_whitespace();
-    for _i in 0..values {
+    for i in 0..values {
         seed = iter.next().unwrap().parse::<usize>().unwrap();
-        collection.push(seed);
+        collection.insert(i,seed);
+//        collection.push(seed);
     }
     let mut lines_number = 0;
     let mut qrs:HashMap<usize,[usize;3]> = HashMap::with_capacity(queries);
@@ -61,7 +63,43 @@ pub fn read_lines() -> (usize,usize,Vec<usize>,HashMap<usize,[usize;3]>) {
     (values,queries,collection,qrs)
 }
 fn main() {
-    let (values,queries,collection,qrs) = read_lines();
+    let (values,queries,mut collection,qrs) = read_lines();
+//    let mut tree = BTreeMap::new();
+//    for (i,elt) in collection.iter() {
+//        *tree
+//            .entry(elt.to_owned())
+//            .or_insert(0) +=1;
+//    }
+//    println!("Tree {:?}",tree);
+    let mut query; 
+    let mut state;
+    for i in 0..queries {
+        query = qrs.get(&i).unwrap(); 
+        if query[0] == 1 {
+            state = collection
+                .get(&(query[1]-1))
+                .unwrap()
+                .to_owned();
+            *collection
+                .get_mut(&(query[1]-1))
+                .unwrap() = query[2];
+//            *tree
+//                .entry(query[2])
+//                .or_insert(0)+=1;
+//            *tree
+//                .entry(state)
+//                .or_insert(0)-=1;
+
+        } else {
+            let mut sum =0;
+            for elt in query[1]-1..=query[2]-1 {
+                sum +=collection.get(&elt).unwrap();
+            }
+            println!("{}",sum);
+
+        }
+    }
+
 /*
     let mut tree = SegmentPoint::build(collection, Add);
     let mut query; 
